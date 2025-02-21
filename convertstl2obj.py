e6c44394d275e4d0350e9e7d4cfc59cb375e13dd
Kevin Vuong <kevinvuong2023@gmail.com>
2025-02-20 22:25:03 -0500

    attempted application of deformables - failed

--- convertstl2obj.py
+++ convertstl2obj.py
@@ -1,8 +1,8 @@
 import trimesh
 
 # Load the STL file
-mesh = trimesh.load_mesh("assets/turtlev1/frontrightflipperlink.STL")
+mesh = trimesh.load_mesh("assets/turtlev1/assets/turtlev1/meshes/frontleftflipperlink.STL")
 
 # Export as OBJ
-mesh.export("assets/turtlev1/frontrightflipperlink.obj")
+mesh.export("assets/turtlev1/assets/turtlev1/meshes/frontleftflipperlink.obj")
 print("Conversion complete: frontrightflipperlink.obj created")
